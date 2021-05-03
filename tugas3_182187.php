9f8ebc1df964357b18ebc037b65e5b4e3b5a5d01
sheyladipa <sheylaaprilia2@gmail.com>
2021-05-03 07:59:01 +0700

    risoles1

---
+++ tugas3_182187.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Heroku</title>
+</head>
+<body>
+<?php
+	echo "Nama    : Sheyla Aprilia";
+	echo "</br>";
+	echo "Kelas   : E (TI)";
+	echo "</br>";
+	echo "Stambuk : 182187";
+?>
+</body>
+</html>
